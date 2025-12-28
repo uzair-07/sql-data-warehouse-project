473c4c8cb970a845c65dddcaae601aaffbc812f3
shaik uzair <101615589+uzair-07@users.noreply.github.com>
2025-12-28 20:11:33 +0100

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,17 @@
+--creating database
+create database datawarehouse;
+go
+
+--using the created database
+use datawarehouse;
+go
+
+--creating schemas
+create schema bronze;
+go
+
+create schema silver;
+go
+
+create schema gold;
+go
